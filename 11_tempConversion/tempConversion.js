fbb9026b77e2ccfb39543f682464226c132f33dd
MarkoJac <47915166+MarkoJac@users.noreply.github.com>
2025-08-13 14:36:02 -0400

    Temperature conversion function complete

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  const degreesCelsius = (farenheit - 32) / 1.8;
+  return Math.round(degreesCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const degreesFarenheit = (celsius * 1.8) + 32;
+  return Math.round(degreesFarenheit * 10) / 10;
 };
 
 // Do not edit below this line
