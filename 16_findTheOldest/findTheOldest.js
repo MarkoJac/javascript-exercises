82b7c0efe23c6047501b9a9af2ee1be96dbc443a
MarkoJac <47915166+MarkoJac@users.noreply.github.com>
2025-08-19 00:34:59 -0400

    Completed exercise 16 find the oldest.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear();
 
+const findTheOldest = function(array) {
+   let peopleAged = array.map(person => {
+    switch (typeof person.yearOfDeath){
+        case "undefined":
+               return ({ name: person.name,
+               age: `${currentYear - person.yearOfBirth}`});
+        case "number":
+               return ({ name: person.name,
+               age: `${person.yearOfDeath - person.yearOfBirth}`});
+    }
+   }
+    ).sort((a, b) => a.age -b.age);
+
+    return peopleAged[peopleAged.length - 1];
+   
 };
 
 // Do not edit below this line
