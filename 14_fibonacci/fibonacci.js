23eecdcc58ac94a144035f357d9ca5eeafff149d
MarkoJac <47915166+MarkoJac@users.noreply.github.com>
2025-08-18 20:25:19 -0400

    Completed exercise 14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const Fn = [0, 1];
+    const num = Number(n);
 
+    if (num === 0) {
+        return 0;
+
+    } else if 
+        (num === 1) {
+        return 1;
+        } 
+
+        else if 
+        (num < 0) {
+        return "OOPS"; 
+
+    }
+
+      else {
+        for (let i = 2; i <= num; i++) {
+          let nextNum = Fn[i-1] + Fn[i-2];
+          Fn.push(nextNum);
+        }
+    }
+    return Fn[num];
+        
+    
 };
 
 // Do not edit below this line
